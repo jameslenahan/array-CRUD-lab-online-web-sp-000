65502744f78a0df709c0aa9b28fb42329618a1a3
James Lenahan <james.k.lenahan@gmail.com>
2019-11-26 17:30:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,11 +10,12 @@ def add_element_to_end_of_array(array, element)
   array = [1, 2, 3, 4]
   array << 5
 end
+
 def add_element_to_start_of_array(array, element)
   array = [1, 2, 3, 4]
   array.unshift = 0
-  array = 
 end
+
 
 def remove_element_from_end_of_array(array)
   
