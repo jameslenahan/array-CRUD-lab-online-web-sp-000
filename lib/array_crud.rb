287a75c33a9f4b996bb4f3dcb6f373df4714d84d
James Lenahan <james.k.lenahan@gmail.com>
2019-11-26 16:18:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,9 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = [1, 2, 3, 4]
+  
   array.push(5)
-  puts array.inspect
+
 end
 def add_element_to_start_of_array(array, element)
   
